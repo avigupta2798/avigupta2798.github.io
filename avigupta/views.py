238820b1b6b02dd0c087230cf484951c95edf98a
Avi Gupta <avibilasgupta@gmail.com>
2021-05-26 18:45:40 +0530

    .

--- avigupta/views.py
+++ avigupta/views.py
@@ -13,37 +13,14 @@ from avigupta.forms import ContactForm
 
 def home(request): # the function will take request as input
     return render(request, 'index.html') # the function then renders an html page template called home.html
-
-
-    def send_mail(self,file_name, kwargs, sum_expected_revenue, to_email):
-        subject = "LOOP: Revenue Sheet Summary for %s %s"%(kwargs['start_date'].strftime("%B"), kwargs['start_date'].year)
-        from_email = EMAIL_HOST_USER
-        to_email = to_email
-        body = 'Dear All,<br/>The summary table given below highlights the expected revenue from each market based on the data entered in the system. Please refer to the attachment for detailed analysis.<br/><br/>'
-        mandi_row_list = []
-        for mandi_obj in sum_expected_revenue.itertuples(index=False):
-            mandi_row= '''<tr>
-                                        <td>%s</td>
-                                        <td>%s</td>
-                                        <td>%s</td>
-                                        <td>%s</td>
-                                    </tr>'''%(mandi_obj.mandi_name, mandi_obj.PickupCost, mandi_obj.loop_payment, mandi_obj.Revenue)
-            mandi_row_list.append(mandi_row)
         
-        '''%(kwargs['end_date'].day, kwargs['end_date'].strftime("%B"), ''.join(mandi_row_list))
-        body= ''.join(body)
-        msg = EmailMultiAlternatives(subject, body, from_email, to_email)
-        msg.content_subtype = "html"
-        msg.attach_file(file_name)
-        msg.send()'''
-
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             subject = "INQUIRY"
             from_email = settings.EMAIL_HOST_USER
-            to_email = form.cleaned_data['email']
+            to_email = [form.cleaned_data['email']]
             body = {
                 'name': form.cleaned_data['name'],
                 'email': form.cleaned_data['email'],
@@ -51,12 +28,7 @@ def contact_view(request):
             }
             body = "\n".join(body.values())
             msg = EmailMultiAlternatives(subject, body, from_email, to_email)
-            msg.content_subtype = "html"
-            try:
-                msg.send()
-            except BadHeaderError:
-                return HttpResponse('Invalid Header Found')
-            return render(reverse('home'))
+            msg.send()
     form = ContactForm()
     return render(request, 'contact.html', {'form':form})
 
