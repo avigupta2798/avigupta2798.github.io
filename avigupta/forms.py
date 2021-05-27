524e4e0005c942905d03886baa19c7ad55461ed7
Avi Gupta <avibilasgupta@gmail.com>
2021-05-27 15:06:00 +0530

    Updated

--- avigupta/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    desc = forms.CharField(required=True, widget=forms.Textarea)
